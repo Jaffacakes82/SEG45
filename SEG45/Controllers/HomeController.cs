88bb8597b18bd1bdd923a2f7ea826dabd25970cd
Joseph Ellis <joseph.ellis@paconsulting.com>
2020-08-04 16:18:01 +0100

    Foo

---
+++ SEG45/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SEG45.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Generate()
+        {
+            return this.View();
+        }
+    }
+}
